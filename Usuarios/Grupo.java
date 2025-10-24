e3d6124d3f56a4455bc9f09dad73393fee257a1b
MCTelles <marcelotellesest@gmail.com>
2025-10-23 21:20:00 -0300

    feat: adicionando atributos para os usuários

--- Usuarios/Grupo.java
+++ Usuarios/Grupo.java
@@ -1,5 +1,11 @@
 package Usuarios;
 
-public class Grupo extends Usuario {
+import java.time.LocalDateTime;
 
+public class Grupo extends Usuario {
+  public Grupo(String id, String nome, String email, String senha, LocalDateTime creationDateTime,
+      String telefone, String endereco, boolean ativo) {
+    super(id, nome, email, senha, creationDateTime, telefone, endereco, ativo);
+    
+  }
 }
