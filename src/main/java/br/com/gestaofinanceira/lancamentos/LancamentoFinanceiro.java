03afd53530b5853dbed586918c6688a63b3fd155
MCTelles <marcelotelles.033@outlook.com>
2025-11-06 23:55:54 -0300

    feat: criando lançamento financeiro

--- src/main/java/br/com/gestaofinanceira/lancamentos/LancamentoFinanceiro.java
+++ src/main/java/br/com/gestaofinanceira/lancamentos/LancamentoFinanceiro.java
@@ -1,5 +1,67 @@
-package lancamentos;
+package Lancamentos;
 
 public class LancamentoFinanceiro {
-  
+    protected String tipo;
+    protected String categoria;
+    protected double valor;
+    protected String data;
+    protected String pagador;
+    protected String beneficiario;
+    protected String recorrencia;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getPagador() {
+        return pagador;
+    }
+
+    public void setPagador(String pagador) {
+        this.pagador = pagador;
+    }
+
+    public String getBeneficiario() {
+        return beneficiario;
+    }
+
+    public void setBeneficiario(String beneficiario) {
+        this.beneficiario = beneficiario;
+    }
+
+    public String getRecorrencia() {
+        return recorrencia;
+    }
+
+    public void setRecorrencia(String recorrencia) {
+        this.recorrencia = recorrencia;
+    }
 }
